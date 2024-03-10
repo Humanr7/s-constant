a941d8631076296b8605e3f62265745efb651730
Evgeniy Hristoforu <136880927+ehristoforu@users.noreply.github.com>
2024-03-10 16:50:16 +0300

    Create webui-user.bat

---
+++ webui-user.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set APPDATA=tmp
+set USERPROFILE=tmp
+set TEMP=tmp
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=--theme dark
+
+@REM Uncomment following code to reference an existing A1111 checkout.
+@REM set A1111_HOME=Your A1111 checkout dir
+@REM
+@REM set VENV_DIR=%A1111_HOME%/venv
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%/embeddings ^
+@REM  --lora-dir %A1111_HOME%/models/Lora
+
+call webui.bat
