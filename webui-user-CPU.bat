2c641203c4d52b6451246f6be9b06d03e3450848
Evgeniy Hristoforu <evgenijhristo87@gmail.com>
2024-03-10 11:56:11 +0300

    First upload

---
+++ webui-user-CPU.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set APPDATA=tmp
+set USERPROFILE=tmp
+set TEMP=tmp
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half
+set PYTORCH_TRACING_MODE=TORCHFX
+set USE_OPENVINO=1
+
+@REM Uncomment following code to reference an existing A1111 checkout.
+@REM set A1111_HOME=Your A1111 checkout dir
+@REM
+@REM set VENV_DIR=%A1111_HOME%/venv
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%/embeddings ^
+@REM  --lora-dir %A1111_HOME%/models/Lora
+
+call webui.bat
